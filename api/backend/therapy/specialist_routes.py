3928c00ba1ec89820c7f7723ca2663a47e40aa82
Connor Tessaro <connorex2@gmail.com>
2025-04-19 21:29:38 -0700

    added db

--- api/backend/therapy/specialist_routes.py
+++ api/backend/therapy/specialist_routes.py
@@ -6,72 +6,70 @@ specialists = Blueprint('specialists', __name__)
 @specialists.route('/specialists', methods=['GET'])
 def get_specialists():
     cursor = db.get_db().cursor()
-    cursor.execute('''SELECT * FROM Specialist''')
+    cursor.execute('''SELECT * FROM Art_Therapy_Specialist''')
     theData = cursor.fetchall()
-    the_response = make_response(jsonify(theData))
-    the_response.status_code = 200
-    return the_response
+    response = make_response(jsonify(theData), 200)
+    return response
 
 @specialists.route('/specialists', methods=['POST'])
-def post_specialists():
+def post_specialist():
     current_app.logger.info('POST /specialists route hit')
-
     new_specialist = request.json
+
     specialistID = new_specialist['Specialist_ID']
     name = new_specialist['Name']
     email = new_specialist['Email']
     phone = new_specialist['Phone']
-    expertise = new_specialist['Expertise']
+    expertise = new_specialist['Art_Specialization']
     bio = new_specialist['Bio']
 
-    insert_query = '''INSERT INTO Specialist 
-    (Specialist_ID, Name, Email, Phone, Expertise, Bio) VALUES
-    (%s, %s, %s, %s, %s, %s)'''
-
+    insert_query = '''
+        INSERT INTO Art_Therapy_Specialist 
+        (Specialist_ID, Name, Email, Phone, Art_Specialization, Bio)
+        VALUES (%s, %s, %s, %s, %s, %s)
+    '''
     data = (specialistID, name, email, phone, expertise, bio)
 
     cursor = db.get_db().cursor()
     cursor.execute(insert_query, data)
     db.get_db().commit()
-    response = make_response(jsonify({"message": "Specialist added", "id": specialistID}), 201)
-    return response
 
-@specialists.route('/specialists/<specialistID>', methods=['GET'])
+    return make_response(jsonify({"message": "Specialist added", "id": specialistID}), 201)
+
+@specialists.route('/specialists/<int:specialistID>', methods=['GET'])
 def get_specialist(specialistID):
     cursor = db.get_db().cursor()
-    cursor.execute('''SELECT * FROM Specialist WHERE Specialist_ID = %s''', (specialistID,))
-    theData = cursor.fetchone()
+    cursor.execute('SELECT * FROM Art_Therapy_Specialist WHERE Specialist_ID = %s', (specialistID,))
+    data = cursor.fetchone()
 
-    if not theData:
+    if not data:
         return make_response(jsonify({"error": "Specialist not found"}), 404)
 
-    the_response = make_response(jsonify(theData), 200)
-    return the_response
+    return make_response(jsonify(data), 200)
 
-@specialists.route('/specialists/<specialistID>', methods=['PUT'])
+@specialists.route('/specialists/<int:specialistID>', methods=['PUT'])
 def update_specialist(specialistID):
-    cursor = db.get_db().cursor()
     updated_specialist = request.json
+
     name = updated_specialist['Name']
     email = updated_specialist['Email']
     phone = updated_specialist['Phone']
-    expertise = updated_specialist['Expertise']
+    expertise = updated_specialist['Art_Specialization']
     bio = updated_specialist['Bio']
 
-    query = '''UPDATE Specialist SET 
+    query = '''
+        UPDATE Art_Therapy_Specialist SET 
         Name = %s,
         Email = %s,
         Phone = %s,
-        Expertise = %s,
+        Art_Specialization = %s,
         Bio = %s
-        WHERE
-        Specialist_ID = %s
+        WHERE Specialist_ID = %s
     '''
-
     data = (name, email, phone, expertise, bio, specialistID)
-    
+
+    cursor = db.get_db().cursor()
     cursor.execute(query, data)
     db.get_db().commit()
-    response = make_response(jsonify({"message": "Specialist updated", 
-                                      "Specialist ID": specialistID}), 200)
-    return response
+
+    return make_response(jsonify({"message": "Specialist updated", "Specialist ID": specialistID}), 200)
